f12602f940a5ca1b423fb6eb069d1ab9062a49a9
paulalovof <paulafernanda.lovo@gmail.com>
2024-03-06 09:14:26 -0300

    migrations prontas no start project

--- Docker/start-project/database/migrations/2024_03_06_120945_create_users_table.php
+++ Docker/start-project/database/migrations/2024_03_06_120945_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('curso_id');
+            $table->foreign('curso_id')->references('id')->on('cursos');
             $table->rememberToken();
             $table->timestamps();
         });
